841eaed2e04be8d4878e4f8f29b1f9acc48df01f
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2023-03-08 18:12:59 +0100

    locale improvement

--- core/src/main/java/io/github/mmm/property/locale/ReadableLocaleProperty.java
+++ core/src/main/java/io/github/mmm/property/locale/ReadableLocaleProperty.java
@@ -6,12 +6,13 @@ import java.util.Locale;
 
 import io.github.mmm.property.ReadableProperty;
 import io.github.mmm.property.object.ReadableSimpleProperty;
+import io.github.mmm.value.observable.locale.ReadableLocaleValue;
 
 /**
  * {@link ReadableProperty} with {@link Locale} {@link #get() value}.
  *
  * @since 1.0.0
  */
-public interface ReadableLocaleProperty extends ReadableSimpleProperty<Locale> {
+public interface ReadableLocaleProperty extends ReadableSimpleProperty<Locale>, ReadableLocaleValue {
 
 }
